3db2e048f9f214e2d97bda68036230d725c6a26d
SheiUn <d215039@outlook.com>
2018-08-30 01:10:14 +0800

    add main file

---
+++ lang-strings.js
@@ -0,0 +1,186 @@
+var MLstrings = [
+    {
+        English: "Repair Page of 2nd Dormitory | Taiwan Tech",
+        繁體中文: "第二學生宿舍報修頁面 | 台科"
+    },
+    {
+        English: "language:",
+        繁體中文: "語言："
+    },
+    {
+        English: "Repair Page of 2nd Dormitory",
+        繁體中文: "第二學生宿舍報修頁面"
+    },
+    {
+        English: "Request",
+        繁體中文: "報修"
+    },
+    {
+        English: "Menu",
+        繁體中文: "目錄"
+    },
+    {
+        English: "Request Repair",
+        繁體中文: "報修"
+    },
+    {
+        English: "Need a repair for your room or other things?",
+        繁體中文: "需要維修你的寢室或其他東西嗎？"
+    },
+    {
+        English: "Request repair service conveniently online.",
+        繁體中文: "線上報修更方便。"
+    },
+    {
+        English: "Look More",
+        繁體中文: "查看更多"
+    },
+    /* Header of Table */
+    {
+        English: "Instant Preview",
+        繁體中文: "即時預覽"
+    },
+    {
+        English: "Notify the dorm center once a week.",
+        繁體中文: "每週送到宿管中心登記一次。"
+    },
+    {
+        English: "Last notifying time:",
+        繁體中文: "上次通知時間："
+    },
+    /* Table */
+    {
+        English: "Timestamp",
+        繁體中文: "時間戳記"
+    },
+    {
+        English: "Informant",
+        繁體中文: "通報人"
+    },
+    {
+        English: "Status",
+        繁體中文: "狀態"
+    },
+    /* Form */
+    {
+        English: "Floor",
+        繁體中文: "樓層"
+    },
+    {
+        English: "Room",
+        繁體中文: "房號"
+    },
+    {
+        English: "Bed",
+        繁體中文: "床號"
+    },
+    {
+        English: "Description",
+        繁體中文: "描述"
+    },
+    /* Map */
+    {
+        English: "Notice",
+        繁體中文: "注意"
+    },
+    {
+        English: "If you are in hurry. Please go to dorm center directly.",
+        繁體中文: "如果你趕時間，請直接到宿管中心。"
+    },
+    {
+        English: "3rd Dormitory 1F",
+        繁體中文: "第三學生宿舍 一樓"
+    },
+    /* Dorm */
+    {
+        English: "Dormitory Cadre",
+        繁體中文: "宿舍幹部"
+    },
+    {
+        English: "If you have any question of accommodation, contact the residence advisor priority.",
+        繁體中文: "如何你有任何住宿上的問題，請優先聯絡樓長。"
+    },
+    {
+        English: "Dorm Manager",
+        繁體中文: "舍長"
+    },
+    {
+        English: "Dorm Vice Manager",
+        繁體中文: "副舍長"
+    },
+    {
+        English: "Resident Assistant",
+        繁體中文: "樓長"
+    },
+    /* Footer */
+    {
+        English: "If you find the bug, you can click the button to report it.",
+        繁體中文: "如果你發現錯誤，你可以點擊按鈕回報。"
+    },
+    {
+        English: "Or you want more information, just go to dorm page.",
+        繁體中文: "或者你想要看到更多的資訊，直接點擊宿舍頁面。"
+    },
+    {
+        English: "Report Bug",
+        繁體中文: "回報錯誤"
+    },
+    {
+        English: "Dorm Page",
+        繁體中文: "宿舍頁面"
+    },
+    /* Side Bar */
+    {
+        English: "Dorm",
+        繁體中文: "宿舍"
+    },
+    {
+        English: "School",
+        繁體中文: "學校"
+    },
+    {
+        English: "Contributor",
+        繁體中文: "貢獻者"
+    },
+    {
+        English: "Front-End",
+        繁體中文: "前端"
+    },
+    {
+        English: "Back-End",
+        繁體中文: "後端"
+    },
+    /* Form */
+    {
+        English: "Request Repair Form",
+        繁體中文: "報修表單"
+    },
+    {
+        English: "-- Floor --",
+        繁體中文: "-- 樓層 --"
+    },
+    {
+        English: "-- Room --",
+        繁體中文: "-- 房間 --"
+    },
+    {
+        English: "-- Bed --",
+        繁體中文: "-- 床位 --"
+    },
+    {
+        English: "Building & Furniture",
+        繁體中文: "土木"
+    },
+    {
+        English: "Plumbing & Electrical",
+        繁體中文: "水電"
+    },
+    {
+        English: "Air Conditioner",
+        繁體中文: "冷氣"
+    },
+    {
+        English: "Others & Not Sure",
+        繁體中文: "其它 & 不確定"
+    },
+];
